313c9c46c12d761b11a568a9b475a6d11e2c0c39
kian <lordkian2000@gmail.com>
2020-05-22 00:18:36 +0430

    init stoLinkCrawler and library

---
+++ stoLinkCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stoLinkCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
